20e58ba107a800730bcc406cb89be30259de67f9
heyimaaditya <aadityatyagi158@gmail.com>
2025-04-14 18:42:47 +0530

    addded code

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://mentorship-backend-g4sf.onrender.com/';
+const API_BASE = 'https://mentorship-backend-g4sf.onrender.com/api';
 
 // Helper function for API requests.
 async function apiRequest(endpoint, method, data = null, token = null) {
